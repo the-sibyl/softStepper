23a5a336e918d79b658126f24ba750a6ee86d94a
EC2 Default User <ec2-user@ip-172-31-33-227.us-east-2.compute.internal>
2017-12-15 19:24:29 +0000

    I made the stepper code more modular, changed the struct fields to be private, added several types of stepping, hold functionality, and direction.

--- softStepper.go
+++ softStepper.go
@@ -30,16 +30,30 @@ import(
 )
 
 type Stepper struct {
-	PinEna *sysfsGPIO.IOPin;
-	PinA *sysfsGPIO.IOPin;
-	PinB *sysfsGPIO.IOPin;
-	PinC *sysfsGPIO.IOPin;
-	PinD *sysfsGPIO.IOPin;
-	StepState int;
-	PulseDuration int;
+	// Enable output pin to the stepper driver IC
+	pinEna *sysfsGPIO.IOPin;
+	// Channel A output pin to the stepper driver IC
+	pinA *sysfsGPIO.IOPin;
+	// Channel B output pin to the stepper driver IC
+	pinB *sysfsGPIO.IOPin;
+	// Channel C output pin to the stepper driver IC
+	pinC *sysfsGPIO.IOPin;
+	// Channel D output pin to the stepper driver IC
+	pinD *sysfsGPIO.IOPin;
+	// Internal flag for the stepper's rotational direction
+	stepDirectionForward bool;
+	// Stepper state. The valid range is 0 to 3.
+	stepState int;
+	// Pulse duration in milliseconds
+	pulseDurationMilliseconds int;
+	// Internal flag for enabling a hold on the stepper. This flag leaves the driver IC enabled and providing
+	// current.
+	holdEnable bool;
 }
 
-func InitStepper(enaPin int, pinA int, pinB int, pinC int, pinD int) *Stepper {
+// Create a Stepper struct with enough data to drive a stepper. Less critical values like HoldEnable may be changed by
+// the user after initialization.
+func InitStepper(enaPin int, pinA int, pinB int, pinC int, pinD int, pulseDurationMilliseconds int) *Stepper {
 	ena, _ := sysfsGPIO.InitPin(enaPin, "out")
 	a, _ := sysfsGPIO.InitPin(pinA, "out")
 	b, _ := sysfsGPIO.InitPin(pinB, "out")
@@ -47,95 +61,126 @@ func InitStepper(enaPin int, pinA int, pinB int, pinC int, pinD int) *Stepper {
 	d, _ := sysfsGPIO.InitPin(pinD, "out")
 
 	stepper := Stepper{
-		PinEna: ena,
-		PinA: a,
-		PinB: b,
-		PinC: c,
-		PinD: d,
-		StepState: 0,
-		PulseDuration: 5,
+		pinEna: ena,
+		pinA: a,
+		pinB: b,
+		pinC: c,
+		pinD: d,
+		stepDirectionForward: true,
+		stepState: 0,
+		pulseDurationMilliseconds: pulseDurationMilliseconds,
+		holdEnable: false,
 	}
 
 	return &stepper
 }
 
-func (s *Stepper) StepForward() {
-	switch s.StepState {
-		case 0:
-			fmt.Println(s.StepState)
-			s.PinA.SetHigh()
-			s.PinB.SetLow()
-			s.PinC.SetHigh()
-			s.PinD.SetLow()
-			s.StepState = 1
-		case 1:
-			fmt.Println(s.StepState)
-			s.PinA.SetLow()
-			s.PinB.SetHigh()
-			s.PinC.SetHigh()
-			s.PinD.SetLow()
-			s.StepState = 2
-		case 2:
-			fmt.Println(s.StepState)
-			s.PinA.SetLow()
-			s.PinB.SetHigh()
-			s.PinC.SetLow()
-			s.PinD.SetHigh()
-			s.StepState = 3
-		case 3:
-			fmt.Println(s.StepState)
-			s.PinA.SetHigh()
-			s.PinB.SetLow()
-			s.PinC.SetLow()
-			s.PinD.SetHigh()
-			s.StepState = 0
-		default:
-			s.StepState = 0
+// Internal generalized step method. The public stepping methods shall call this method.
+func (s *Stepper) step(numSteps int) {
+	// There are four states total. States will overflow forward or backward, e.g. incrementing state 3 results in
+	// state 0, and decrementing state 0 results in state 3.
+
+	if numSteps < 1 {
+		fmt.Println("Warning: An invalid number of steps was specified.")
+		return
 	}
 
-	s.PinEna.SetHigh()
-	time.Sleep(time.Millisecond * time.Duration(s.PulseDuration))
-	s.PinEna.SetLow()
+	for k := 0; k < numSteps; k++ {
+
+		// Forward direction case
+		if s.stepDirectionForward == true {
+			if s.stepState < 3 {
+				s.stepState++
+			} else {
+				s.stepState = 0
+			}
+
+		// Reverse direction case
+		} else {
+			if s.stepState > 0 {
+				s.stepState--
+			} else {
+				s.stepState = 3
+			}
+		}
+
+		// Set the pin outputs based on the new state
+		switch s.stepState {
+			case 0:
+				fmt.Println(s.stepState)
+				s.pinA.SetHigh()
+				s.pinB.SetLow()
+				s.pinC.SetHigh()
+				s.pinD.SetLow()
+			case 1:
+				fmt.Println(s.stepState)
+				s.pinA.SetLow()
+				s.pinB.SetHigh()
+				s.pinC.SetHigh()
+				s.pinD.SetLow()
+			case 2:
+				fmt.Println(s.stepState)
+				s.pinA.SetLow()
+				s.pinB.SetHigh()
+				s.pinC.SetLow()
+				s.pinD.SetHigh()
+			case 3:
+				fmt.Println(s.stepState)
+				s.pinA.SetHigh()
+				s.pinB.SetLow()
+				s.pinC.SetLow()
+				s.pinD.SetHigh()
+			default:
+				panic("Code error: default stepper state was reached.")
+		}
+
+		// Now that the new stepper state is driven on the output pins, assert the enable signal so that the driver IC
+		// will provide current to the motor.
+		s.pinEna.SetHigh()
+		time.Sleep(time.Millisecond * time.Duration(s.pulseDurationMilliseconds))
+	}
+
+	// If the stepper is in holding mode, keep the enable pin asserted so that the coils continue to be driven with
+	// the present state.
+	if s.holdEnable == false {
+		s.pinEna.SetLow()
+	}
 }
 
+// Run the stepper forward by one step
+func (s *Stepper) StepForward() {
+	s.stepDirectionForward = true
+	s.step(1)
+}
 
+// Run the stepper backward by one step
 func (s *Stepper) StepBackward() {
-	switch s.StepState {
-		case 0:
-			fmt.Println(s.StepState)
-			s.PinA.SetHigh()
-			s.PinB.SetLow()
-			s.PinC.SetLow()
-			s.PinD.SetHigh()
-			s.StepState = 1
-		case 1:
-			fmt.Println(s.StepState)
-			s.PinA.SetLow()
-			s.PinB.SetHigh()
-			s.PinC.SetLow()
-			s.PinD.SetHigh()
-			s.StepState = 2
-		case 2:
-			fmt.Println(s.StepState)
-			s.PinA.SetLow()
-			s.PinB.SetHigh()
-			s.PinC.SetHigh()
-			s.PinD.SetLow()
-			s.StepState = 3
-		case 3:
-			fmt.Println(s.StepState)
-			s.PinA.SetHigh()
-			s.PinB.SetLow()
-			s.PinC.SetHigh()
-			s.PinD.SetLow()
-			s.StepState = 0
-		default:
-			s.StepState = 0
-	}
+	s.stepDirectionForward = false
+	s.step(1)
+}
 
-	s.PinEna.SetHigh()
-	time.Sleep(time.Millisecond * time.Duration(s.PulseDuration))
-//	s.PinEna.SetLow()
+// Run the stepper forward by a specified number of steps
+func (s *Stepper) StepForwardMulti(numSteps int) {
+	s.stepDirectionForward = true
+	s.step(numSteps)
+}
+
+// Run the stepper backward by a specified number of steps
+func (s *Stepper) StepBackwardMulti(numSteps int) {
+	s.stepDirectionForward = false
+	s.step(numSteps)
+}
+
+// Enable stepper holding. Note: this usually consumes a large amount of energy.
+func (s *Stepper) EnableHold() {
+	s.holdEnable = true
+	s.pinEna.SetHigh()
+}
+
+// Disable stepper holding
+func (s *Stepper) DisableHold() {
+	s.holdEnable = false
+	s.pinEna.SetLow()
 }
 
 func test_main() {
@@ -143,7 +188,7 @@ func test_main() {
 //	stepper2 := InitStepper(30, 21, 22, 23, 24)
 //	stepper3 := InitStepper(31, 26, 27, 28, 29)
 
-	stepper1 := InitStepper(2, 3, 4, 17, 27)
+	stepper1 := InitStepper(2, 3, 4, 17, 27, 5)
 
 //	stepper2 := InitStepper(, 5, 6, 13, 19)
 //	stepper3 := InitStepper(, 12, 16, 20, 21)
